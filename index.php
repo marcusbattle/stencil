fb6788b692df4c1ebf25bc1cbdaee51d5c52ab09
Marcus Battle <marcus@marcusbattle.com>
2014-04-10 16:55:49 -0400

    switch dom for index.php

--- index.php
+++ index.php
@@ -51,11 +51,11 @@
 
       <div class="container">
          <div class="row">
-            <div class="col-md-3">
-               <?php dynamic_sidebar( 'Left Sidebar' ); ?>
-            </div>
             <div class="col-md-9">
                <?php get_template_part( 'parts/content' ); ?>
+            </div>
+            <div class="col-md-3">
+               <?php dynamic_sidebar( 'Left Sidebar' ); ?>
             </div>
          </div>
       </div>
