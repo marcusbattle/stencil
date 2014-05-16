4928ff3c73fa01585dac9258a9022931487cf772
Marcus Battle <marcus@marcusbattle.com>
2014-05-15 22:29:51 -0400

    added menu to index.php

--- index.php
+++ index.php
@@ -27,36 +27,50 @@
       <?php echo get_option('google_tag_manager'); ?>
       
       <?php if ( has_nav_menu( 'contact-menu' ) ): ?>
-      <header class="contact">
-         <div class="container">
-            <div class="row">
-               <?php wp_nav_menu( array( 'theme_location' => 'contact-menu', 'container' => false, 'menu_class' => 'contact-menu right' ) ); ?>
-            </div>
-         </div>
-      </header>
-      <?php endif; ?>
-
-      <div class="container">
-         <div class="row">
-            <div class="branding col-md-3 col-sm-12 col-xs-12">
-               <a class="logo" href="<?php echo home_url(); ?>"><img src="<?php echo get_bloginfo('template_url'); ?>/assets/newjc/newjc-logo-small.png" /></a>
-               <div class="title hide">
-                  <h1><?php echo get_bloginfo(); ?></h1>
-                  <span><?php echo get_bloginfo( 'description' ) ?></span>
+         <header class="contact container-fluid hidden-xs">
+            <div class="container">
+               <div class="row">
+                  <?php wp_nav_menu( array( 'theme_location' => 'contact-menu', 'container' => false, 'menu_class' => 'contact-menu right' ) ); ?>
                </div>
             </div>
-            <div class="col-md-9">
-               <?php if ( has_nav_menu( 'header-menu' ) ) 
-                  wp_nav_menu( array( 'theme_location' => 'header-menu', 'container' => false, 'menu_class' => 'nav nav-pills header-menu' ) ); 
-               ?>
-               <?php if ( $parent_id ): ?>
-                  <ul class="nav nav-pills">
-                     <?php wp_list_pages("title_li=&child_of=$parent_id"); ?>
-                  </ul>
-               <?php endif; ?>
+         </header><!-- header.contact -->
+      <?php endif; ?>
+
+      <div id="navbar" class="container-fluid">
+         <div class="container">
+            <div class="row">
+               <div class="branding col-md-3 col-sm-12 col-xs-12 hidden-xs">
+                  <a class="logo" href="<?php echo home_url(); ?>"><img src="<?php echo get_bloginfo('template_url'); ?>/assets/newjc/newjc-logo-small.png" /></a>
+                  <div class="title hide">
+                     <h1><?php echo get_bloginfo(); ?></h1>
+                     <span><?php echo get_bloginfo( 'description' ) ?></span>
+                  </div>
+               </div>
+               <div class="col-md-9 col-xs-12">
+                  <?php if ( has_nav_menu( 'header-menu' ) ): ?>
+                     <?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container' => false, 'menu_class' => 'nav nav-pills header-menu hidden-xs hidden-s', 'walker' => new HeaderMenuWalker ) ); ?>
+                     <div class="navbar navbar-inverse hidden-lg hidden-md hidden-sm" role="navigation" style="margin-left: -30px; margin-right: -30px; position: fixed; width: 100%; z-index: 99; top: 0;">
+                        <div class="container">
+                           <div class="navbar-header">
+                              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                                 <span class="sr-only">Toggle navigation</span>
+                                 <span class="icon-bar"></span>
+                                 <span class="icon-bar"></span>
+                                 <span class="icon-bar"></span>
+                              </button>
+                              <a class="navbar-brand" href="#"><?php echo get_bloginfo(); ?></a>
+                           </div>
+                           <div class="collapse navbar-collapse">
+                              <?php if ( has_nav_menu( 'contact-menu' ) ) wp_nav_menu( array( 'theme_location' => 'contact-menu', 'container' => false, 'menu_class' => 'nav navbar-nav contact-menu' ) ); ?>
+                              <?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container' => false, 'menu_class' => 'nav navbar-nav', 'walker' => new HeaderMenuWalker ) ); ?>
+                           </div><!--/.nav-collapse -->
+                        </div>
+                     </div>
+                  <?php endif; ?>
+               </div>
             </div>
          </div>
-      </div>
+      </div><!-- #navbar -->
 
       <div class="container">
          <div class="row">
