8303c0439da4124525579269adcf780c61f65d7f
Marcus Battle <marcus@marcusbattle.com>
2014-05-17 19:19:39 -0400

    css updates

--- index.php
+++ index.php
@@ -1,110 +1,35 @@
 <?php
+
+   if ( !is_post_type_archive() ) {
    
-   $post_id = $post->ID;
-   $parent_id = ($post->post_parent) ? $post->post_parent : $post_id;
+      $post_id = $post->ID;
+      $parent_id = ($post->post_parent) ? $post->post_parent : $post_id;
 
-   $page_layout = (get_post_meta( $post_id, '_page_layout', true )) ? get_post_meta( $post_id, '_page_layout', true ) : 'right-sidebar';
+      $page_layout = (get_post_meta( $post_id, '_page_layout', true )) ? get_post_meta( $post_id, '_page_layout', true ) : 'right-sidebar';
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-   <head>
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-      <title><?php echo get_bloginfo(); ?></title>
+   }
 
-      <?php wp_head(); ?>
+   get_header();
+   
+   get_template_part( 'parts/nav' ); ?>
 
-      <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-      <!--[if lt IE 9]>
-         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-      <![endif]-->
-   </head>
-   <body>
-      <?php echo get_option('google_tag_manager'); ?>
-      
-      <?php if ( has_nav_menu( 'contact-menu' ) ): ?>
-         <header class="contact container-fluid hidden-xs">
-            <div class="container">
-               <div class="row">
-                  <?php wp_nav_menu( array( 'theme_location' => 'contact-menu', 'container' => false, 'menu_class' => 'contact-menu right' ) ); ?>
-               </div>
-            </div>
-         </header><!-- header.contact -->
-      <?php endif; ?>
-
-      <div id="navbar" class="container-fluid">
-         <div class="container">
-            <div class="row">
-               <div class="branding col-md-3 col-sm-12 col-xs-12 hidden-xs">
-                  <a class="logo" href="<?php echo home_url(); ?>"><img src="<?php echo get_bloginfo('template_url'); ?>/assets/newjc/newjc-logo-small.png" /></a>
-                  <div class="title hide">
-                     <h1><?php echo get_bloginfo(); ?></h1>
-                     <span><?php echo get_bloginfo( 'description' ) ?></span>
-                  </div>
-               </div>
-               <div class="col-md-9 col-xs-12">
-                  <?php if ( has_nav_menu( 'header-menu' ) ): ?>
-                     <?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container' => false, 'menu_class' => 'nav nav-pills header-menu hidden-xs hidden-s', 'walker' => new HeaderMenuWalker ) ); ?>
-                     <div class="navbar navbar-inverse hidden-lg hidden-md hidden-sm" role="navigation" style="margin-left: -30px; margin-right: -30px; position: fixed; width: 100%; z-index: 99; top: 0;">
-                        <div class="container">
-                           <div class="navbar-header">
-                              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                                 <span class="sr-only">Toggle navigation</span>
-                                 <span class="icon-bar"></span>
-                                 <span class="icon-bar"></span>
-                                 <span class="icon-bar"></span>
-                              </button>
-                              <a class="navbar-brand" href="#"><?php echo get_bloginfo(); ?></a>
-                           </div>
-                           <div class="collapse navbar-collapse">
-                              <?php if ( has_nav_menu( 'contact-menu' ) ) wp_nav_menu( array( 'theme_location' => 'contact-menu', 'container' => false, 'menu_class' => 'nav navbar-nav contact-menu' ) ); ?>
-                              <?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container' => false, 'menu_class' => 'nav navbar-nav', 'walker' => new HeaderMenuWalker ) ); ?>
-                           </div><!--/.nav-collapse -->
-                        </div>
-                     </div>
-                  <?php endif; ?>
-               </div>
-            </div>
-         </div>
-      </div><!-- #navbar -->
-
-      <div class="container">
-         <div class="row">
-            <div class="page-header col-md-12">
-               <?php if ( is_post_type_archive() ): ?>
-                  <h1><?php post_type_archive_title(); ?></h1>
-               <?php else: ?>
-                  <h1><?php the_title(); ?></h1>
-               <?php endif; ?>
-            </div>
+   <div class="container">
+      <div class="row">
+         <div class="page-header col-md-12">
+            <?php if ( is_post_type_archive() ): ?>
+               <h1><?php post_type_archive_title(); ?></h1>
+            <?php else: ?>
+               <h1><?php the_title(); ?></h1>
+            <?php endif; ?>
          </div>
       </div>
+   </div>
 
-      <div class="container <?php echo $page_layout ?>">
-         <?php 
-            if ( $page_layout ) get_template_part( 'parts/' . $page_layout ); 
-            else get_template_part( 'parts/right-sidebar' );
-         ?>
-      </div>
+   <div class="container <?php echo $page_layout ?>">
+      <?php 
+         if ( $page_layout ) get_template_part( 'parts/' . $page_layout ); 
+         else get_template_part( 'parts/right-sidebar' );
+      ?>
+   </div>
 
-      <?php //if ( is_active_sidebar( 'Footer Main' ) ) : ?>
-         <div class="container">
-            <div class="row">
-               <?php dynamic_sidebar( 'Footer' ); ?>
-            </div>
-         </div>
-      <?php //endif; ?>
-
-      <footer class="container">
-         <div class="row">
-            <p>&copy; <?php echo date('Y'); ?> <?php echo get_bloginfo(); ?>. All Rights Reserved.</p>
-         </div>
-      </footer>
-      
-      <?php wp_footer(); ?>
-   </body>
-</html>
+<?php get_footer(); ?>
