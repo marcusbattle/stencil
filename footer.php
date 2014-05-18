a128edd21107d9115c56869c9e228686f745dea7
Marcus Battle <marcus@marcusbattle.com>
2014-05-17 21:24:45 -0400

    Updated footer

--- footer.php
+++ footer.php
@@ -1,29 +1,33 @@
-      <div id="footer-widgets" class="container-fluid">
-         <div class="container">
-            <div class="row">
-               <?php dynamic_sidebar( 'Footer' ); ?>
-            </div>
-         </div>
-      </div>
-
-      <?php if ( has_nav_menu('footer-menu') ): ?>
-         <div id="footer-menu" class="container-fluid">
+      <footer>
+         <div id="footer-widgets" class="container-fluid">
             <div class="container">
                <div class="row">
-                  <?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'container' => false ) ); ?>
+                  <?php dynamic_sidebar( 'Footer' ); ?>
                </div>
             </div>
-         </div><!-- #footer-menu -->
-      <?php endif; ?>
+         </div>
 
-      <footer class="container-fluid">
-         <div class="container">
-            <div class="row">
-               <p>&copy; <?php echo date('Y'); ?> <?php echo get_bloginfo(); ?>. All Rights Reserved.</p>
+         <?php if ( has_nav_menu('footer-menu') ): ?>
+            <div id="footer-menu" class="container-fluid">
+               <div class="container">
+                  <div class="row">
+                     <?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'container' => false ) ); ?>
+                  </div>
+               </div>
+            </div><!-- #footer-menu -->
+         <?php endif; ?>
+
+         <div id="copyright" class="container-fluid">
+            <div class="container">
+               <div class="row">
+                  <div class="col-md-12">
+                     <p>&copy; <?php echo date('Y'); ?> <?php echo get_bloginfo(); ?>. All Rights Reserved.</p>
+                  </div>
+               </div>
             </div>
          </div>
       </footer>
-      
+
       <?php wp_footer(); ?>
    </body>
 </html>
