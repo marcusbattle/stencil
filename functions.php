0f6070ba62243451d23cda66a8cee8af03068178
Marcus Battle <marcus@marcusbattle.com>
2014-05-24 01:23:07 -0400

    fixed breadcrumbs

--- functions.php
+++ functions.php
@@ -324,44 +324,84 @@
 	}
 
 	function the_breadcrumb() {
+	    
 	    global $post;
-	    echo '<ul class="breadcrumbs">';
-	    if (!is_home()) {
-	        echo '<li><a href="';
-	        echo get_option('home');
-	        echo '">';
-	        echo 'Home';
-	        echo '</a></li><li class="separator"> / </li>';
-	        if (is_category() || is_single()) {
-	            echo '<li>';
-	            the_category(' </li><li class="separator"> / </li><li> ');
-	            if (is_single()) {
-	                echo '</li><li class="separator"> / </li><li>';
-	                the_title();
-	                echo '</li>';
-	            }
-	        } elseif (is_page()) {
-	            if($post->post_parent){
-	                $anc = get_post_ancestors( $post->ID );
-	                $title = get_the_title();
-	                foreach ( $anc as $ancestor ) {
-	                    $output = '<li><a href="'.get_permalink($ancestor).'" title="'.get_the_title($ancestor).'">'.get_the_title($ancestor).'</a></li> <li class="separator">/</li>';
-	                }
-	                echo $output;
-	                echo '<strong title="'.$title.'"> '.$title.'</strong>';
-	            } else {
-	                echo '<li><strong> '.get_the_title().'</strong></li>';
-	            }
-	        }
+
+	    $post_types = get_post_types( array( '_builtin' => true ), 'names' );
+	    $queried_object = get_queried_object();
+ 	
+ 		// echo "<pre>";
+ 		// print_r( $queried_object );
+ 		// echo "</pre>";
+
+	    $breadcrumb = '<ul class="breadcrumb">';
+
+	    if ( !is_home() )
+	    	$breadcrumb .= '<li><a href="' . home_url() . '">Home</a></li>';
+
+	    if ( is_singular() && !in_array( get_post_type(), $post_types ) ) {
+
+	    	$post_type_object = get_post_type_object( get_post_type() );
+
+	    	if ( $post_type_object->has_archive )
+	    		$breadcrumb .= '<li><a href="' . home_url( $post_type_object->rewrite['slug'] ) . '">' . $post_type_object->label . '</a></li>';
+
 	    }
-	    elseif (is_tag()) {single_tag_title();}
-	    elseif (is_day()) {echo"<li>Archive for "; the_time('F jS, Y'); echo'</li>';}
-	    elseif (is_month()) {echo"<li>Archive for "; the_time('F, Y'); echo'</li>';}
-	    elseif (is_year()) {echo"<li>Archive for "; the_time('Y'); echo'</li>';}
-	    elseif (is_author()) {echo"<li>Author Archive"; echo'</li>';}
-	    elseif (isset($_GET['paged']) && !empty($_GET['paged'])) {echo "<li>Blog Archives"; echo'</li>';}
-	    elseif (is_search()) {echo"<li>Search Results"; echo'</li>';}
-	    echo '</ul>';
+
+	    if ( is_singular() && $post->post_parent ) {
+
+	    	foreach( get_post_ancestors( $post->ID ) as $parent )
+	    		$breadcrumb .= '<li><a href="' . get_permalink( $parent ) . '">' . get_the_title( $parent ) . '</a></li>';
+
+	    }
+
+
+	    if ( is_page() || is_single() )
+	    	$breadcrumb .= '<li>' . get_the_title() . '</li>';
+
+	    if ( is_post_type_archive() )
+	    	$breadcrumb .= '<li>' . post_type_archive_title( '', false ) . '</li>';
+
+	    $breadcrumb .= '</ul>';
+
+	    echo $breadcrumb;
+
+	//     if (!is_home()) {
+	//         echo '<li><a href="';
+	//         echo get_option('home');
+	//         echo '">';
+	//         echo 'Home';
+	//         echo '</a></li><li class="separator"> / </li>';
+	//         if (is_category() || is_single()) {
+	//             echo '<li>';
+	//             the_category(' </li><li class="separator"> / </li><li> ');
+	//             if (is_single()) {
+	//                 echo '</li><li class="separator"> / </li><li>';
+	//                 the_title();
+	//                 echo '</li>';
+	//             }
+	//         } elseif (is_page()) {
+	//             if($post->post_parent){
+	//                 $anc = get_post_ancestors( $post->ID );
+	//                 $title = get_the_title();
+	//                 foreach ( $anc as $ancestor ) {
+	//                     $output = '<li><a href="'.get_permalink($ancestor).'" title="'.get_the_title($ancestor).'">'.get_the_title($ancestor).'</a></li> <li class="separator">/</li>';
+	//                 }
+	//                 echo $output;
+	//                 echo '<strong title="'.$title.'"> '.$title.'</strong>';
+	//             } else {
+	//                 echo '<li><strong> '.get_the_title().'</strong></li>';
+	//             }
+	//         }
+	//     }
+	//     elseif (is_tag()) {single_tag_title();}
+	//     elseif (is_day()) {echo"<li>Archive for "; the_time('F jS, Y'); echo'</li>';}
+	//     elseif (is_month()) {echo"<li>Archive for "; the_time('F, Y'); echo'</li>';}
+	//     elseif (is_year()) {echo"<li>Archive for "; the_time('Y'); echo'</li>';}
+	//     elseif (is_author()) {echo"<li>Author Archive"; echo'</li>';}
+	//     elseif (isset($_GET['paged']) && !empty($_GET['paged'])) {echo "<li>Blog Archives"; echo'</li>';}
+	//     elseif (is_search()) {echo"<li>Search Results"; echo'</li>';}
+	//     echo '</ul>';
 	}
 
 
