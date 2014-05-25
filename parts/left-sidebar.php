4464aa739021b01720394df62127136f17df294a
Marcus Battle <marcus@marcusbattle.com>
2014-05-24 22:44:34 -0400

    fixed sidebar templates to display widgets

--- parts/left-sidebar.php
+++ parts/left-sidebar.php
@@ -1,6 +1,6 @@
 <div class="row">
 	<div class="col-md-3">
-	   <?php dynamic_sidebar( 'Left Sidebar' ); ?>
+	   <?php dynamic_sidebar( 'Sidebar' ); ?>
 	</div>
 	<div class="col-md-9">
 	   <?php get_template_part( 'parts/content' ); ?>
--- parts/right-sidebar.php
+++ parts/right-sidebar.php
@@ -1,11 +1,11 @@
 <div class="row">
-	<div class="col-md-9"> 
+	<div class="col-md-7"> 
 		<?php 
 			if ( is_post_type_archive() ) get_template_part( 'parts/post/loop' ); 
 			else get_template_part( 'parts/page/content' ); 
 		?>
 	</div>
-	<div class="col-md-3">
-	   	<?php dynamic_sidebar( 'Right Sidebar' ); ?>
+	<div class="col-md-4 col-md-offset-1">
+	   	<?php dynamic_sidebar( 'Sidebar' ); ?>
 	</div>
 </div>
