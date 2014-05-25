b31eb20fc195335df48014e583b4ddf1d00f2f55
Marcus Battle <marcus@marcusbattle.com>
2014-05-25 00:38:50 -0400

    fixed dom

--- archive.php
+++ archive.php
@@ -3,26 +3,13 @@
 	get_template_part( 'parts/nav' );
 ?>
 
-   <div id="page-title" class="container-fluid blank">
-      <div class="container">
-         <div class="row">
-            <div class="page-header col-md-12">
-               <?php if ( is_post_type_archive() ): ?>
-                  <h1><?php post_type_archive_title(); ?></h1>
-               <?php else: ?>
-                  <h1><?php the_title(); ?></h1>
-               <?php endif; ?>
-            </div>
-         </div>
-      </div>
-   </div>
-
+   <div class="peep-hole container-fluid"></div>
+   
    <div id="main" class="container-fluid">
       <div class="container <?php echo $page_layout ?>">
-         <?php
-            the_breadcrumb();
-			get_template_part( 'parts/post/loop' );
-         ?>
+         <?php the_breadcrumb(); ?>
+         <h1><?php post_type_archive_title(); ?></h1>
+         <?php get_template_part( 'parts/post/loop' ); ?>
       </div>
    </div>
 
